6663d24115cc5d77873ee45e7360a5c86122f3de
mihai.maier <mihai.maier1991@gmail.com>
2023-03-23 14:53:16 +0200

    show project

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -5,7 +5,7 @@
     <v-card-title v-html="title" />
 
     <v-card-actions>
-      <v-btn @click="show = !show"> Show </v-btn>
+      <v-btn :href="web" target="_blank"> Show </v-btn>
 
       <v-spacer></v-spacer>
 
@@ -39,6 +39,10 @@ export default {
       default: "",
     },
     link: {
+      type: String,
+      default: "",
+    },
+    web: {
       type: String,
       default: "",
     },
